f02e257c57109c02abc13d8be7417d69e689c717
Abdullahi Gbadamosi <obagbadamosi@gmail.com>
2024-07-03 00:37:44 +0100

    First Commit

---
+++ MergeDocuments.cs
@@ -0,0 +1,81 @@
+﻿using PDFA_Conversion;
+using ServiceReference2;
+using System.ServiceModel;
+
+namespace MergeDocumentsSample
+{
+    class MergeDocuments{
+        static void Main(string[] args){
+            DocumentConverterServiceClient? client = null;
+            string[] sourceFiles = { "C:\\Converter\\test1.pdf", "C:\\Converter\\test2.docx" };
+
+            try{
+                Console.WriteLine("Merging...");
+                ProcessingOptions processingOptions = getProcessingOptions(sourceFiles);
+                client = UtilClass.OpenService();
+                BatchResults results = client.ProcessBatch(processingOptions);
+                byte[] mergedFile = results.Results[0].File;
+                /* string hour = DateTime.Now.Hour.ToString();
+                 string minute = DateTime.Now.Minute.ToString();
+                 string second = DateTime.Now.Second.ToString(); */
+
+                //  string destinationFileName = "C:\\Converter\\MergedOutput"+hour+minute+second+".pdf";
+                string destinationFileName = "C:\\Converter\\MergedOutput.pdf";
+                using (FileStream fs = File.Create(destinationFileName))
+                {
+                    fs.Write(mergedFile, 0, mergedFile.Length);
+                    fs.Close();
+                }
+                Console.WriteLine("...to "+ destinationFileName);
+            }
+
+            catch (FaultException<WebServiceFaultException> ex){
+                Console.WriteLine("FaultException occurred: ExceptionType: " + ex.Detail.ExceptionType.ToString());
+            }
+
+            catch (Exception ex){
+                Console.WriteLine(ex.ToString());
+            }
+
+            finally{
+                if (client != null)
+                    UtilClass.CloseService(client);
+            }
+        }
+
+        private static ProcessingOptions getProcessingOptions(string[] sourceFileNames) {
+            ProcessingOptions processingOptions = new ProcessingOptions();
+
+            MergeSettings mergeSettings = new MergeSettings();
+            mergeSettings.BreakOnError = false;
+            processingOptions.MergeSettings = mergeSettings;
+
+            List<SourceFile> sourceFileList = new List<SourceFile>();
+            foreach (string sourceFileName in sourceFileNames) {
+                byte[] fileContent = File.ReadAllBytes(sourceFileName);
+
+                OpenOptions openOptions = new OpenOptions();
+                openOptions.OriginalFileName = Path.GetFileName(sourceFileName);
+                openOptions.FileExtension = Path.GetExtension(sourceFileName);
+
+                ConversionSettings conversionSettings = new ConversionSettings();
+                conversionSettings.Fidelity = ConversionFidelities.Full;
+                conversionSettings.Quality = ConversionQuality.OptimizeForPrint;
+
+                FileMergeSettings fileMergeSettings = new FileMergeSettings();
+                fileMergeSettings.TopLevelBookmark = openOptions.OriginalFileName;
+
+                SourceFile sourceFile = new SourceFile();
+                sourceFile.OpenOptions = openOptions;
+                sourceFile.ConversionSettings = conversionSettings;
+                sourceFile.MergeSettings = fileMergeSettings;
+                sourceFile.File = fileContent;
+                sourceFileList.Add(sourceFile);
+                Console.WriteLine(sourceFileName);
+            }
+
+            processingOptions.SourceFiles = sourceFileList.ToArray();
+            return processingOptions;
+        }
+    }
+}
---
+++ UtilClass.cs
@@ -0,0 +1,60 @@
+﻿using ServiceReference2;
+using System;
+using System.ServiceModel;
+
+namespace PDFA_Conversion
+{
+
+    public class UtilClass
+    {
+        // ** The URL where the Web Service is located. Amend host name if needed.
+        static string SERVICE_URL = "http://localhost:41734/Muhimbi.DocumentConverter.WebService/";
+
+        /// <summary>
+        /// Configure the Bindings, endpoints and open the service using the specified address.
+        /// </summary>
+        /// <returns>An instance of the Web Service.</returns>
+        public static DocumentConverterServiceClient OpenService()
+        {
+            DocumentConverterServiceClient? client = null;
+
+            try
+            {
+                BasicHttpBinding binding = new BasicHttpBinding();
+                // ** Use standard Windows Security.
+                binding.Security.Mode = BasicHttpSecurityMode.TransportCredentialOnly;
+                binding.Security.Transport.ClientCredentialType =
+                                                                HttpClientCredentialType.Windows;
+                // ** Increase the client Timeout to deal with (very) long running requests.
+                binding.SendTimeout = TimeSpan.FromMinutes(120);
+                binding.ReceiveTimeout = TimeSpan.FromMinutes(120);
+
+                // ** Set the maximum document size to 50MB
+                binding.MaxReceivedMessageSize = 50 * 1024 * 1024;
+                binding.ReaderQuotas.MaxArrayLength = 50 * 1024 * 1024;
+                binding.ReaderQuotas.MaxStringContentLength = 50 * 1024 * 1024;
+
+                EndpointAddress epa = new EndpointAddress(new Uri(SERVICE_URL));
+                client = new DocumentConverterServiceClient(binding, epa);
+                client.Open();
+
+                return client;
+            }
+            catch (Exception)
+            {
+                CloseService(client);
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Check if the client is open and then close it.
+        /// </summary>
+        /// <param name="client">The client to close</param>
+        public static void CloseService(DocumentConverterServiceClient client)
+        {
+            if (client != null && client.State == CommunicationState.Opened)
+                client.Close();
+        }
+    }
+}
